5bfe7910a95eafe966f68ea837d14f39fa7ac5c7
Kyle <k90armstrong@gmail.com>
2017-11-03 17:14:44 -0600

    adds more beggining functionality

--- assets/javascript/welcome.js
+++ assets/javascript/welcome.js
@@ -12,11 +12,13 @@ var db = firebase.database();
 
 // classes
 class User {
-    constructor(name, id, inGame, request) {
+    constructor(name, id, inGame, request, wins, losses) {
         this.username = name;
         this.id = id;
         this.inGame = inGame;
         this.request = request;
+        this.wins = wins;
+        this.losses = losses;
     }
 }
 
@@ -33,7 +35,7 @@ $(document).ready(function () {
         $('input').val(userName);
     }
 
-    var user = new User(userName, userId, false, false);
+    var user = new User(userName, userId, false, false, 0, 0);
     var userRef = db.ref('/availableUsers/' + user.id);
 
 
@@ -49,7 +51,10 @@ $(document).ready(function () {
             var fireUserRef = availableUsers.push({
                 name: user.username,
                 inGame: false,
-                request: false
+                request: false,
+                wins: 0,
+                losses: 0,
+                invitation: 'waiting'
             });
             user.id = fireUserRef.key;
             // set the id for local storage
@@ -59,9 +64,12 @@ $(document).ready(function () {
             // set in game to false and request to false
             userRef.update({
                 inGame: false,
-                request: false
+                request: false,
+                invitation: 'waiting'
             });
         }
+        availableUsers.on('child_added', availableUsersHandler);
+        userRef.on('value', userValueChangeHandler);
         var $fadeElement = $('.modal-content');
         fadeOut($fadeElement, function () {
             $fadeElement.find('form').addClass('display-none');
@@ -70,9 +78,67 @@ $(document).ready(function () {
                 setTimeout(function () {
                     fadeOut($('.modal'), function () {
                         $('.modal').removeClass('is-active');
+                        $('.game-rooms').removeClass('display-none');
                     });
                 }, 2000);
             });
+        });
+    }
+
+    function userValueChangeHandler(snapshot) {
+        var dbUser = snapshot.val();
+        console.log(dbUser);
+        // if dbUser.request == true then bring up a modal to accept the invitation
+    }
+
+    function availableUsersHandler(snapshot) {
+        console.log(snapshot);
+        var dbUser = snapshot.val();
+        dbUser.id = snapshot.key;
+        console.log(dbUser);
+        if (dbUser.id !== user.id && !dbUser.inGame) {
+            var $el = createUserElement(dbUser);
+            $('tbody').append($el);
+        }
+    }
+
+    function createUserElement(dbUser) {
+        var $tr = $('<tr>');
+        var $td = $('<td>');
+        $td.text(dbUser.name);
+        $tr.append($td);
+
+        var $td = $('<td>');
+        $td.text(dbUser.wins);
+        $tr.append($td);
+
+        var $td = $('<td>');
+        $td.text(dbUser.losses);
+        $tr.append($td);
+
+        var $btn = $('<button>').text('Play Me!');
+        $btn.addClass('button play-button');
+        $btn.attr('data-id', dbUser.id);
+        var $td = $('<td>');
+        $td.append($btn);
+        $tr.append($td);
+
+        return $tr;
+    }
+
+    function playButtonHanlder(event) {
+        var id = $(this).attr('data-id');
+        // invite a user
+        db.ref('/availableUsers/' + id).update({
+            request: true
+        });
+        // add listener to the user that was invited
+        db.ref('/availableUsers/' + id).on('value', function (snapshot) {
+            var oponent = snapshot.val();
+            if (oponent.invitation === 'accepted') {
+                // start game!
+                console.log('start game');
+            }
         });
     }
 
@@ -106,6 +172,9 @@ $(document).ready(function () {
     }
 
     // listeners
-    $('button').on('click', submittButtonHandler)
+    $('.submit-button').on('click', submittButtonHandler);
+    $(document).on('click', '.play-button', playButtonHanlder)
+
+
 
 });
